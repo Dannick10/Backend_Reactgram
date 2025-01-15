287d41e5d1d02c864c8f0283da6efe1eb7e47c84
Dannick010 <danmaggot2@gmail.com>
2025-01-15 19:25:45 -0300

    fix: update user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,6 @@
 const User = require("../models/User");
 const bycript = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { default: mongoose } = require("mongoose");
-const { use } = require("../routes/UserRoutes");
 const mongoose = require("mongoose")
 
 const jwtSecret = process.env.JWT_SECRET;
@@ -90,38 +88,51 @@ const getCurrentUser = async(req,res) => {
 }
 
 const update = async(req,res) => {
- 
-  const {name, password, bio} = req.body
   
-  let profileImage = null
+    const { name, password, bio } = req.body;
+    let profileImage = null;
+
+   
+    if (req.file) {
+      profileImage = req.file.filename;
+    }
+
+    const reqUser = req.user;
+
+
+    const user = await User.findById(reqUser._id).select("-password");
+    if (!user) {
+      return res.status(404).json({ error: "Usuário não encontrado." });
+    }
+
+
+    if (name) {
+      user.name = name;
+    }
+
+    if (password) {
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+      user.password = passwordHash;
+    }
+
+    if (profileImage) {
+      user.profileImage = profileImage;
+    }
+
+    if (bio) {
+      user.bio = bio;
+    }
+
+    await user.save();
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    });
   
-  if(req.file) {
-      profileImage = req.file.filename 
-  }
-
-  const reqUser = req.user
-
-  const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select("-password") 
-
-  if(name) {
-    user.name = name
-  }
-
-  if(password) {
-    const salt = await bycript.genSalt();
-    const passwordHash = await bycript.hash(password, salt);
-
-    user.password = passwordHash
-  }
-
-  if(bio) {
-      user.bio = bio
-  }
-
-
-  await user.save()
-
-  res.status(200).json(user)
 }
 
 module.exports = {
